d5ff4ba4d68c5d7ef042de987b7bf70ab0748b05
skr090850 <skr090850@gmail.com>
2025-08-13 17:06:44 +0530

    All Complete

--- Simple Calculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ Simple Calculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.simplecalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,58 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtFirst, edtSecond;
+    Button btnAdd, btnSub, btnMulti, btnDiv;
+    TextView txtResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        edtFirst = findViewById(R.id.firstEdit);
+        edtSecond = findViewById(R.id.SecondEdit);
+        btnAdd = findViewById(R.id.add);
+        btnSub = findViewById(R.id.sub);
+        btnMulti = findViewById(R.id.multi);
+        btnDiv = findViewById(R.id.div);
+        txtResult = findViewById(R.id.result);
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(edtFirst.getText().toString());
+                int num2= Integer.parseInt(edtSecond.getText().toString());
+                txtResult.setText(String.valueOf(num1+num2));
+            }
+        });
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(edtFirst.getText().toString());
+                int num2= Integer.parseInt(edtSecond.getText().toString());
+                txtResult.setText(String.valueOf(num1-num2));
+            }
+        });
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(edtFirst.getText().toString());
+                int num2= Integer.parseInt(edtSecond.getText().toString());
+                txtResult.setText(String.valueOf(num1*num2));
+            }
+        });
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float num1 = Integer.parseInt(edtFirst.getText().toString());
+                float num2= Integer.parseInt(edtSecond.getText().toString());
+                txtResult.setText(String.valueOf(num1/num2));
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
