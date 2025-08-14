4a86c07ca7ae764c2d8ecbae17770700d2445e17
skr090850 <skr090850@gmail.com>
2025-08-14 18:33:01 +0530

    UI Update

--- Simple Calculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ Simple Calculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -13,56 +13,49 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText edtFirst, edtSecond;
-    Button btnAdd, btnSub, btnMulti, btnDiv;
+    Button btnAdd, btnSub, btnMulti, btnDiv, btnAc, btnPercentage, btn1, btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnDot,btnBackspace,btnEqualToo;
     TextView txtResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        edtFirst = findViewById(R.id.firstEdit);
-        edtSecond = findViewById(R.id.SecondEdit);
-        btnAdd = findViewById(R.id.add);
-        btnSub = findViewById(R.id.sub);
-        btnMulti = findViewById(R.id.multi);
-        btnDiv = findViewById(R.id.div);
-        txtResult = findViewById(R.id.result);
 
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int num1 = Integer.parseInt(edtFirst.getText().toString());
-                int num2= Integer.parseInt(edtSecond.getText().toString());
-                txtResult.setText(String.valueOf(num1+num2));
-            }
-        });
-        btnSub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int num1 = Integer.parseInt(edtFirst.getText().toString());
-                int num2= Integer.parseInt(edtSecond.getText().toString());
-                txtResult.setText(String.valueOf(num1-num2));
-            }
-        });
-        btnMulti.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int num1 = Integer.parseInt(edtFirst.getText().toString());
-                int num2= Integer.parseInt(edtSecond.getText().toString());
-                txtResult.setText(String.valueOf(num1*num2));
-            }
-        });
-        btnDiv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                float num1 = Integer.parseInt(edtFirst.getText().toString());
-                float num2= Integer.parseInt(edtSecond.getText().toString());
-                txtResult.setText(String.valueOf(num1/num2));
-            }
-        });
+//        txtResult = findViewById(R.id.result);
+//
+//
+//        btnAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int num1 = Integer.parseInt(edtFirst.getText().toString());
+//                int num2= Integer.parseInt(edtSecond.getText().toString());
+//                txtResult.setText(String.valueOf(num1+num2));
+//            }
+//        });
+//        btnSub.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int num1 = Integer.parseInt(edtFirst.getText().toString());
+//                int num2= Integer.parseInt(edtSecond.getText().toString());
+//                txtResult.setText(String.valueOf(num1-num2));
+//            }
+//        });
+//        btnMulti.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int num1 = Integer.parseInt(edtFirst.getText().toString());
+//                int num2= Integer.parseInt(edtSecond.getText().toString());
+//                txtResult.setText(String.valueOf(num1*num2));
+//            }
+//        });
+//        btnDiv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                float num1 = Integer.parseInt(edtFirst.getText().toString());
+//                float num2= Integer.parseInt(edtSecond.getText().toString());
+//                txtResult.setText(String.valueOf(num1/num2));
+//            }
+//        });
 
 
 
