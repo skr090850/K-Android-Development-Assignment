d40139aa3d388ba17ce7f874979bd9c033a01631
skr090850 <skr090850@gmail.com>
2025-08-22 11:41:19 +0530

    UI Complete

--- AuthAppUsingKotlin/app/src/main/java/com/example/authappusingkotlin/SignInActivity.kt
+++ AuthAppUsingKotlin/app/src/main/java/com/example/authappusingkotlin/SignInActivity.kt
@@ -2,29 +2,26 @@ package com.example.authappusingkotlin
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SignUpActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        val signInTextbtn = findViewById<TextView>(R.id.signinTextBtn);
-        signInTextbtn.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
+        setContentView(R.layout.activity_sign_in)
+        val signUpTextbtn = findViewById<TextView>(R.id.signUpTextBtn);
+        signUpTextbtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
