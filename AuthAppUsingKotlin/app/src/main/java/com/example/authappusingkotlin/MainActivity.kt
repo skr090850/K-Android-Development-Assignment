5e56a3b716f99b1dbcfdabeaa42c6cf992f03949
skr090850 <skr090850@gmail.com>
2025-08-22 11:28:24 +0530

    Signup Ui complete

--- AuthAppUsingKotlin/app/src/main/java/com/example/authappusingkotlin/MainActivity.kt
+++ AuthAppUsingKotlin/app/src/main/java/com/example/authappusingkotlin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.authappusingkotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val authbtn = findViewById<Button>(R.id.authBtn);
+        authbtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- AuthAppUsingKotlin/app/src/main/java/com/example/authappusingkotlin/SignUpActivity.kt
+++ AuthAppUsingKotlin/app/src/main/java/com/example/authappusingkotlin/SignUpActivity.kt
@@ -16,5 +16,6 @@ class SignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
